d4b99ad4f9f7485b376f92822d9433d6ce0df189
joumarhiba <93978029+joumarhiba@users.noreply.github.com>
2022-02-21 21:18:41 +0100

    Add files via upload

---
+++ views/index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/styles/styleindex.css">
+	<title>Home Page</title>
+</head>
+<body>
+<main class="main">
+	<aside class="sidebar">
+	  <nav class="nav">
+		<ul>
+			<img class="logo" src="/public/assets/new_logo-bl.png" alt="le logo de la communauté d'amateurs des films et des séries">
+		  <li class="active"><a href="#">Bienvenue dans Notre Communauté</a></li>
+		  <li><a href="./index.php">Accueil</a></li>
+		  <li><a href="./posts/viewPosts.php">Voir Publications</a></li>
+		  <li><a href="#">Gérer Les Publications</a></li>
+		</ul>
+	  </nav>
+	  <footer class="social-media">
+		<a href="http://instagram.com"><img class="icon" src="https://img.icons8.com/ios/50/ffffff/instagram-new--v1.png"/></a>
+		<a href="http://facebook.com"><img class="icon" src="https://img.icons8.com/ios/50/ffffff/facebook-circled--v1.png"/></a>
+		<a href="http://youtube.com"><img class="icon" src="https://img.icons8.com/ios/50/ffffff/youtube-play--v1.png"/></a>
+	</footer>
+	</aside>
+	<aside class="sidebar-mobil">
+		<nav class="nav-mobil">
+		  <ul>
+			  <img class="logo" src="/public/assets/new_logo-bl.png" alt="le logo de la communauté d'amateurs des films et des séries">
+			<li class="active"><a href="#">Bienvenue </a></li>
+			<li><a href="../index.php"><img class="accueil" src="https://img.icons8.com/ios-glyphs/30/ffffff/home.png"/></a></li>
+			<li><a href="../posts/viewPosts.php"><img src="https://img.icons8.com/ios/50/ffffff/show-all-views.png"/></a></li>
+			<li><a href="#"><img src="https://img.icons8.com/ios/50/ffffff/edit-calendar.png"/></a></li>
+		  </ul>
+		</nav>
+		<footer class="social-media-mobil">
+		  <a href="http://instagram.com"><img class="icon" src="https://img.icons8.com/ios/50/ffffff/instagram-new--v1.png"/></a>
+		  <a href="http://facebook.com"><img class="icon" src="https://img.icons8.com/ios/50/ffffff/facebook-circled--v1.png"/></a>
+		  <a href="http://youtube.com"><img class="icon" src="https://img.icons8.com/ios/50/ffffff/youtube-play--v1.png"/></a>
+	  </footer>
+	  </aside>
+  </main>
+  <section class="head">
+	  <div class="background1">
+		 <!--Great Meeting are just the beginning  -->
+		<!-- <p> Show your LOVE of movies.</p> -->
+	  </div>
+	  <div class="background2"></div>
+	  <a class="btn" href="/app/views/login.php">Créer Un Compte</a>
+  </section>
+
+  <section>
+	  <!-- comédie / drama/aventure / fantastique / action / horreur -->
+	  <h2> Nos Catégories</h2>
+	  <h2 class="s-titre">Films</h2>
+	  <div class="movies">
+		<div class="ctg ">
+			<img src="/public/assets/comedy.jpg" alt="">
+			<div class="content">
+				<h4>Comedie</h4>
+			</div>
+		</div>
+		<div class="ctg">
+			<img src="/public/assets/adventure.jpg" alt="">
+			<div class="content">
+				<h4>Aventure</h4>
+			</div>
+		</div>
+		<div class="ctg">
+			<img src="/public/assets/actn.jpg" alt="">
+			<div class="content">
+				<h4>Action</h4>
+			</div>
+		</div>
+		<div class="ctg">
+			<img src="/public/assets/fantastique.jpg" alt="">
+			<div class="content">
+				<h4>Fantastique</h4>
+			</div>
+		</div>
+		</div>
+
+		
+		<h2 class="s-titre">Séries</h2>
+		<div class="series">
+			<div class="ctg ">
+				<img src="/public/assets/comedy_s.png" alt="">
+				<div class="content">
+					<h4>Comedie</h4>
+				</div>
+			</div>
+			<div class="ctg">
+				<img src="/public/assets/aventure_s.jpeg" alt="">
+				<div class="content">
+					<h4>Aventure</h4>
+				</div>
+			</div>
+			<div class="ctg">
+				<img src="/public/assets/action_s.jpg" alt="">
+				<div class="content">
+					<h4>Action</h4>
+				</div>
+			</div>
+			<div class="ctg">
+				<img src="/public/assets/fantasy._sjpg.jpg" alt="">
+				<div class="content">
+					<h4>Fantastique</h4>
+				</div>
+			</div>
+			</div>
+	
+	
+  </section>
+  <section class="about-us">
+	  <h2>Qui Sommes Nous ? </h2>
+	  <p class="about-us-para"><span><strong>Ciné Master</strong></span> est une communauté d'amateurs des films et des séries, où chacun peut s'inscrire, créer son propre post sur son film /sa série préféré et partager son point de vue concernant d'autres posts, partager vos derniers épisodes regardés, et découvrez des nouveautés… <br><br> Avec <strong>Ciné Master</strong>, suivez, réagissez, commentez, et inspirez-vous
+		de l’activité de vos amis. </p>
+  </section>
+</body>	
+</html>
